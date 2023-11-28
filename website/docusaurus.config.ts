98a87bd41ff02e60fba40c8c99114a36859d22c1
Gitcoins <gitcoins@outlook.com>
2023-11-28 09:55:27 +0800

    Update docusaurus.config.ts

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -398,7 +398,7 @@ export default async function createConfigAsync() {
               isDeployPreview ||
               isBranchDeploy ||
               isBuildFast
-                ? getLastVersion()
+                ? "current"
                 : getLastVersion(),
 
             onlyIncludeVersions: (() => {
