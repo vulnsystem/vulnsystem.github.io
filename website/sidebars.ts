c44dbde428262ca08a42dc3fbab0571567603220
Gitcoins <gitcoins@outlook.com>
2024-07-01 19:20:15 +0800

    Update sidebars.ts

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -92,6 +92,7 @@ const sidebars: SidebarsConfig = {
         'docusaurus-docustips',
         'docusaurus-createdirectory',
         'docusaurus-addimage',
+        'docusaurus-diagrams',
       ],
     },
   ],
